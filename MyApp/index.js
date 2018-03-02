30dd427bb4c0851f184a27fc014185c77ab75d90
ShruthiRajanna <shruthi.rajanna@mindtree.com>
2018-03-02 05:34:46 +0000

    Update index.js
    updated message

--- MyApp/index.js
+++ MyApp/index.js
@@ -16,7 +16,7 @@ var router = express.Router();
  *         description: Success
  */
 router.get('/', function(req, res, next) {
-  res.send({ "Message": 'Hi!!........... .........hiiiii ' });
+  res.send({ "Message": 'good morning ' });
 });
 
 module.exports = router;
